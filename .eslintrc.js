d3d3ffe54d1cc197dff05be27c31151253fd8cda
Pedro Ernesto Perdoná Rech <p.perdona@gmail.com>
2019-06-23 20:17:31 -0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: 'airbnb',
+  rules: {
+    'linebreak-style': ['error', 'windows'],
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+  },
+};
